700df978ec849e4511e6d7577326749c8f2f50aa
heyyitskamran <farooquialtamash2005@gmail.com>
2025-12-22 19:13:28 +0530

    created utils rapers for eas

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv"
 import connectDB from "./db/db.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
@@ -8,6 +9,18 @@ dotenv.config({
 
 
 connectDB()
+.then(() =>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`);
+        app.on("Error", (error) => {
+            console.log("Error", error)
+            throw error
+        })
+    })
+})
+.catch((err) => {
+    console.log("Mongo Db connection failed !!!", err)
+})
 
 
 /*import mongoose from "mongoose";
---
+++ src/utils/ApiError.js
@@ -0,0 +1,25 @@
+class ApiErro extends Error{
+    constructor(
+        statusCode,
+        message= "somthing went wrong",
+        errors= [],
+        statck= ""
+
+    ){
+        super(message)
+        this.statusCode= statusCode
+        this.data= null
+        this.message = message
+        this.success= false
+        this.errors= errors
+
+        if(statck){
+            this.stack = statck
+        }
+        else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
+
+export {ApiErro}
